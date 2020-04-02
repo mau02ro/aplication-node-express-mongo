82a52312e148d24b0d941cfc3ce19d13b110ef10
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-04-01 22:10:41 -0400

    primeras rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('hola index');
+    res.render('index');
 });
 
 router.get('/about', (req, res) => {
-    res.send('Hola about');
+    res.render('about');
 })
 
 module.exports = router;
